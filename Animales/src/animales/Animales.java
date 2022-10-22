b8c8f986a403859f347cf3be85648d63d08d50f0
DiegoCort <81386375+DiegoCort@users.noreply.github.com>
2022-10-22 15:20:51 -0500

    Create Animales.java

---
+++ Animales/src/animales/Animales.java
@@ -0,0 +1,16 @@
+
+package animales;
+
+public abstract class Animales {
+
+    protected String Sonido;
+    protected String Alimentos;
+    protected String Habitad;
+    protected String NombreCientifico;
+    
+    public abstract String getSonido();
+    public abstract String getAlimentos();
+    public abstract String getSHabitad();
+    public abstract String getNombreCientifico();
+    
+}
