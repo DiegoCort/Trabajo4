58813e573ef152c67510480e72b5a432eb888e4b
DiegoCort <81386375+DiegoCort@users.noreply.github.com>
2022-10-22 15:38:03 -0500

    Create Leon.java

---
+++ Animales/src/animales/Leon.java
@@ -0,0 +1,20 @@
+
+package animales;
+public class Leon extends Felino {
+
+    public String getSonido() {
+        return"Rugodo";
+    }
+
+    public String getAlimentos() {
+        return"Carnivoro";
+    }
+
+    public String getSHabitad() {
+        return"Praderas";
+    }
+
+    public String getNombreCientifico() {
+        return"Panthera leo";
+    } 
+}
