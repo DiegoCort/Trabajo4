2bf25d234176f56dc7210e5197b3f2e13f11c8a0
DiegoCort <81386375+DiegoCort@users.noreply.github.com>
2022-10-22 15:21:00 -0500

    Create Perro.java

---
+++ Animales/src/animales/Perro.java
@@ -0,0 +1,21 @@
+
+package animales;
+public class Perro extends Canido {
+
+    public String getSonido() {
+        return"Ladrido";
+    }
+ 
+    public String getAlimentos() {
+        return"Carnivoro";
+    }
+
+    public String getSHabitad() {
+        return"Domestico";
+    }
+
+    public String getNombreCientifico() {
+        return"Canis lupus familiaris";
+    }
+    
+}
