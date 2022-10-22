5b3b5256a0d403afb1dc5c49db5366a7b0a8dd68
DiegoCort <81386375+DiegoCort@users.noreply.github.com>
2022-10-22 15:38:06 -0500

    Create Gato.java

---
+++ Animales/src/animales/Gato.java
@@ -0,0 +1,23 @@
+
+package animales;
+
+
+public class Gato extends Felino {
+
+    public String getSonido() {
+        return"Maullido";
+    }
+
+    public String getAlimentos() {
+        return"Ratones";
+    }
+
+    public String getSHabitad() {
+        return"Domestico";
+    }
+
+    public String getNombreCientifico() {
+        return"Felis silvestris catus";
+    } 
+    
+}
