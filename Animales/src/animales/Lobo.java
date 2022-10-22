bf1c4a6137ef237c757bbfdb99cef70180ef5683
DiegoCort <81386375+DiegoCort@users.noreply.github.com>
2022-10-22 15:21:02 -0500

    Create Lobo.java

---
+++ Animales/src/animales/Lobo.java
@@ -0,0 +1,20 @@
+package animales;
+
+public class Lobo extends Canido {
+   
+    public String getSonido() {
+        return"Aullido";
+    }
+
+    public String getAlimentos() {
+        return"Carnivoro";
+    }
+
+    public String getSHabitad() {
+        return"Bosque";
+    }
+
+    public String getNombreCientifico() {
+        return"Canis lupus";
+    } 
+}
