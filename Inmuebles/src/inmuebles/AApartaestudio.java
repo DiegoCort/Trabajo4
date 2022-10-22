f0c53a9dc27b5bfe3b9d310ae2b7355522f38eea
DiegoCort <81386375+DiegoCort@users.noreply.github.com>
2022-10-22 14:10:19 -0500

    Create AApartaestudio.java

---
+++ Inmuebles/src/inmuebles/AApartaestudio.java
@@ -0,0 +1,19 @@
+
+package inmuebles;
+
+
+public class AApartaestudio extends vApartamento {
+    
+    protected static double vArea=1500000;
+    public AApartaestudio(int identificadorInmobiliario, int areaMetrosCaudrados, String direccion, 
+        int nHabitaciones, int nBaños) {
+        
+        super(identificadorInmobiliario, areaMetrosCaudrados, direccion, 
+        1, 1);
+    }
+    
+    public void ImprimirAApartaestudio(){
+        
+        super.ImprimirvApartameno();
+    }   
+}
