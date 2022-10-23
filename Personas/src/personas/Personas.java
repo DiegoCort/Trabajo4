8a67a297010ef257790b6f7a94cc90275500e2f3
DiegoCort <81386375+DiegoCort@users.noreply.github.com>
2022-10-22 23:25:19 -0500

    Create Personas.java

---
+++ Personas/src/personas/Personas.java
@@ -0,0 +1,17 @@
+
+package personas;
+
+
+public class Personas {
+
+    String Nombre, Apellidos, Telefono, Direccion;
+    public Personas(String Nombre, String Apellidos, String Telefono,
+    String Direcion){
+        
+        this.Apellidos = Apellidos;
+        this.Direccion = Direccion;
+        this.Nombre = Nombre;
+        this.Telefono = Telefono;
+        
+    }
+}
